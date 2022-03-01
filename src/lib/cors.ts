11a5ea5200d32ad0dfe376d3fcd3c6475f4d0c28
idbd <idbd.young@gmail.com>
2022-03-01 13:23:27 +0900

    Add cors whitelist

--- src/lib/cors.ts
+++ src/lib/cors.ts
@@ -1,7 +1,15 @@
 import cors from 'cors';
 
+const whiteList = ['http://localhost:3000'];
+
 const corsOptions = {
-  origin: process.env.CLIENT_ORIGIN,
+  origin: (origin, cb) => {
+    if (whiteList.indexOf(origin) !== -1) {
+      cb(null, true);
+    } else {
+      cb(new Error('Not allowed origin!'));
+    }
+  },
 };
 const Cors = cors(corsOptions);
 
